cde289ea41fc34e9034cd0e051389cd063a3c14c
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-01-06 22:31:13 +0000

    Owner commands

---
+++ cogs/owner.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+
+class Owner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, cog):
+        """
+        Reloads a cog and updates changes to it
+        """
+        try:
+            self.bot.reload_extension("cogs." + cog)
+            self.bot.dispatch("load", cog)
+        except Exception as error:
+            await ctx.send(f"```py\n{error}```")
+            return
+        await ctx.send("✅")
+        print(f"------------Reloaded {cog}------------")
+
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
