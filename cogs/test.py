fcf856fa6c600c8c3b1f684c9a8cea2e69ebde11
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-01-06 17:40:46 +0000

    Added in role adding from Select Menu

--- cogs/test.py
+++ cogs/test.py
@@ -8,12 +8,18 @@ class Test(discord.ui.View):
         super().__init__()
         self.bot = bot
 
-    @discord.ui.select(custom_id="Testing", placeholder="Testing", min_values=1, max_values=1, options=[
-        discord.SelectOption(label="First option", value="Linux", emoji="linux:927948388853506138"),
+    @discord.ui.select(custom_id="Testing", placeholder="Testing", min_values=1, max_values=3, options=[
+        discord.SelectOption(label="First option", value="Python", emoji="python:927948531229139014"),
         discord.SelectOption(label="Second option", value="Ruby", emoji="ruby:927947448528273408"),
         discord.SelectOption(label="Third option", value="Arch", emoji="arch:927947448788353104")])
     async def callback(self, select: discord.ui.Select, interaction: discord.Interaction):
-        await interaction.response.send_message(f"You Selected {select.values}  !", ephemeral=False)
+        if select.values[0] == "Python":
+            Role = self.bot.guild.get_role(927942689381552138)
+            User = await self.bot.guild.fetch_member(interaction.user.id)
+            await User.add_roles(Role, reason="Reaction Role.")
+            await interaction.response.send_message(f"You Selected python and I've added you to the role!", ephemeral=False)
+        else:
+            await interaction.response.send_message(f"L", epheremal=False)
 
 
 class TestingCog(commands.Cog, name="TestingCog"):
