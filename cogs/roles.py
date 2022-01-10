74e8042f607073f69d7fc26110d5901439ec29db
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-01-10 19:10:10 +0000

    Roles

--- cogs/roles.py
+++ cogs/roles.py
@@ -17,8 +17,25 @@ LANGUAGE_VIEW = {
     "Typescript": {"roleId": 928780873267040266, "emoji": "typescript:927947446301106257"},
 }
 
+INTEREST_VIEW = {
+    "Programming": {"roleId": 928786544440000522, "emoji": "code:928786531081150495"},
+    "3D Printing": {"roleId": 928723382751592518, "emoji": "3dprinting:928790227248635945"},
+    "Cyber Security": {"roleId": 928724530673901669, "emoji": "firewall:928790564953010266"},
+    "Networking": {"roleId": 928785182406873188, "emoji": "router:928791011675762798"},
+    "Circuits": {"roleId": 928724312502988820, "emoji": "transistor:927947446791831602"}
+}
 
-class ReactView(discord.ui.View):
+OS_VIEW = {
+    "Linux": {"roleId": 928722740821753937, "emoji": "linux:927948388853506138"},
+    "MacOS": {"roleId": 928722779161890816, "emoji": "apple:927947449170026496"},
+    "Windows": {"roleId": 928722827765481502, "emoji": "windows:927947446565371904"},
+    "Debian/Ubuntu": {"roleId": 928736847755116574, "emoji": "debian:927947446875734096"},
+    "Arch": {"roleId": 928737067851206696, "emoji": "arch:927947448788353104"},
+    "BSD": {"roleId": 928780279802372136, "emoji": "bsd:927947447139958795"}
+}
+
+
+class LangReactView(discord.ui.View):
     def __init__(self, ctx: commands.Context):
         super().__init__()
         self.ctx = ctx
@@ -38,6 +55,46 @@ class ReactView(discord.ui.View):
         await interaction.channel.purge(limit=1)  # Deletes interaction message once done
 
 
+class InterestsReactView(discord.ui.View):
+    def __init__(self, ctx: commands.Context):
+        super().__init__()
+        self.ctx = ctx
+
+    @discord.ui.select(custom_id="Interest Reaction Menu", placeholder="Please Select Your Interest.",
+                       min_values=1, max_values=12,
+                       options=[discord.SelectOption(label=name, emoji=value["emoji"])
+                                for name, value in INTEREST_VIEW.items()])
+    async def callback(self, select: discord.ui.Select, interaction: discord.Interaction):
+        for interest in select.values:
+            role_id = INTEREST_VIEW[interest]["roleId"]
+            role = self.ctx.guild.get_role(role_id)
+            await self.ctx.author.add_roles(role, reason=f"Reaction role.")
+
+        interests = ", ".join(select.values)
+        await interaction.response.send_message(f"You chose: {interests}!", ephemeral=True)
+        await interaction.channel.purge(limit=1)  # Deletes interaction message once done
+
+
+class OSReactView(discord.ui.View):
+    def __init__(self, ctx: commands.Context):
+        super().__init__()
+        self.ctx = ctx
+
+    @discord.ui.select(custom_id="Operating System Reaction Menu", placeholder="Please Select Your Operating Systems.",
+                       min_values=1, max_values=12,
+                       options=[discord.SelectOption(label=name, emoji=value["emoji"])
+                                for name, value in OS_VIEW.items()])
+    async def callback(self, select: discord.ui.Select, interaction: discord.Interaction):
+        for os in select.values:
+            role_id = OS_VIEW[os]["roleId"]
+            role = self.ctx.guild.get_role(role_id)
+            await self.ctx.author.add_roles(role, reason=f"Reaction role.")
+
+        osystems = ", ".join(select.values)
+        await interaction.response.send_message(f"You chose: {osystems}!", ephemeral=True)
+        await interaction.channel.purge(limit=1)  # Deletes interaction message once done
+
+
 class ReactionCreate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot  # type: commands.Bot
@@ -47,7 +104,21 @@ class ReactionCreate(commands.Cog):
     async def langrole(self, ctx: commands.Context):
         """Creates a role picker for languages"""
         await ctx.message.delete(delay=2)  # Deletes command in chat
-        await ctx.send("Select your language roles below.", view=ReactView(ctx))
+        await ctx.send(view=LangReactView(ctx))
+
+    @commands.command(message_command=False, slash_command=True, ephemeral=True,
+                      slash_command_guilds=[925804557001437184])
+    async def interestrole(self, ctx: commands.Context):
+        """Creates a role picker for languages"""
+        await ctx.message.delete(delay=2)  # Deletes command in chat
+        await ctx.send(view=InterestsReactView(ctx))
+
+    @commands.command(message_command=False, slash_command=True, ephemeral=True,
+                      slash_command_guilds=[925804557001437184])
+    async def osrole(self, ctx: commands.Context):
+        """Creates a role picker for languages"""
+        await ctx.message.delete(delay=2)  # Deletes command in chat
+        await ctx.send(view=OSReactView(ctx))
 
 
 def setup(bot):
