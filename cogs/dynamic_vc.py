48133c66a77808489fc889b368c44a1ef70c2b4c
Quantum-Cucumber <null>
2022-01-15 20:38:49 +1100

    Add dynamic vc

---
+++ cogs/dynamic_vc.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from config import DYN_VC_ID
+
+
+class DynamicVC(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.channels = []  # all temp VCs
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member,
+                                    before: discord.VoiceState, after: discord.VoiceState):
+        if before.channel != after.channel and after.channel:  # if they join a voice channel
+            if after.channel.id == DYN_VC_ID:  # if they join the DVC channel
+                # Duplicate the channel to retain permissions and parent - set name
+                vc = await after.channel.category.create_voice_channel(name="Temp Voice")
+                self.channels.append(vc.id)  # save the ID
+                await member.move_to(vc)
+        elif before.channel and before.channel.id in self.channels:  # if they left a chat we made
+            if len(before.channel.members) == 0:  # if channel is empty
+                self.channels.remove(before.channel.id)  # remove channel from DynVC list
+                await before.channel.delete()  # delete the actual channel
+
+
+def setup(bot):
+    bot.add_cog(DynamicVC(bot))
