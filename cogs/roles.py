2742b20e22fe1db428631deae810c91aafeadbd6
Quantum-Cucumber <quantumcucumber@protonmail.com>
2022-01-19 09:41:52 +1100

    Use new emotes

--- cogs/roles.py
+++ cogs/roles.py
@@ -30,12 +30,12 @@ LANGUAGE_VIEW = {
 
 INTEREST_DIVIDER = 928722640867319859
 INTEREST_VIEW = {
-    "3D Printing": {"roleId": 928723382751592518, "emoji": "3dprinting:931974624747610222"},
-    "Circuits": {"roleId": 928724312502988820, "emoji": "transistor:931974800795127898"},
-    "Cyber Security": {"roleId": 928724530673901669, "emoji": "firewall:931974706855292948"},
-    "Game Dev/Modding": {"roleId": 933008738103742464, "emoji": ":gamedev:933010131980025946"},
-    "Networking": {"roleId": 928785182406873188, "emoji": "router:931974766003376159"},
-    "Programming": {"roleId": 928786544440000522, "emoji": "code:932828643951001670"},
+    "3D Printing": {"roleId": 928723382751592518, "emoji": "3dprinting:933127502711361607"},
+    "Circuits": {"roleId": 928724312502988820, "emoji": "transistor:933129000941912144"},
+    "Cyber Security": {"roleId": 928724530673901669, "emoji": "firewall:933127502921105408"},
+    "Game Dev/Modding": {"roleId": 933008738103742464, "emoji": "gamedev:933127502451331093"},
+    "Networking": {"roleId": 928785182406873188, "emoji": "router:933127503902543883"},
+    "Programming": {"roleId": 928786544440000522, "emoji": "code:933127502963048488"},
 }
 
 OS_DIVIDER = 928737228539174992
