a825ee17d7c61cda8fab394a49e0612dd1eb5826
QuantumCucumber <quantumcucumber@protonmail.com>
2023-10-25 23:06:14 +1100

    Detect external role changes to apply divider to

--- cogs/roles.py
+++ cogs/roles.py
@@ -40,24 +40,13 @@ class RoleDropdown(discord.ui.Select):
             elif name not in self.values and role in member.roles:  # Not selected and is assigned
                 await self.member.remove_roles(role, reason="Reaction role.")
 
-        # Assign divider if given and needed
-        if self.divider is not None:
-            divider_role = self.member.guild.get_role(self.divider)
-            if self.values and divider_role not in member.roles:
-                await self.member.add_roles(divider_role, reason="Divider role.")
-            elif not self.values and divider_role in member.roles:
-                await self.member.remove_roles(divider_role, reason="Divider role.")
-
         # Respond with selection
         await interaction.response.send_message("Updated roles!", ephemeral=True)
 
 
 class RoleMenu(discord.ui.View):
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__(timeout=None)
-
-        with open("roles.yaml") as fd:
-            config = yaml.full_load(fd)
 
         for name, values in config.items():
             def make_callback(name_, values_):
@@ -81,6 +70,9 @@ class Roles(commands.Cog):
     def __init__(self, bot):
         self.bot = bot  # type: commands.Bot
 
+        with open("roles.yaml") as fd:
+            self.role_config = yaml.full_load(fd)
+
     @discord.message_command(name="Attach role menu")
     @discord.default_permissions(manage_messages=True)
     async def attach_roles(self, ctx: discord.ApplicationContext, message: discord.Message):
@@ -90,12 +82,61 @@ class Roles(commands.Cog):
         if message.author.id != self.bot.user.id:
             await ctx.respond("The selected message must have been sent by the bot", ephemeral=True)
         else:
-            await message.edit(view=RoleMenu())
+            await message.edit(view=RoleMenu(self.role_config))
             await ctx.respond("Attached", ephemeral=True)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.bot.add_view(RoleMenu())
+        self.bot.add_view(RoleMenu(self.role_config))
+
+    @commands.Cog.listener()
+    async def on_member_update(self, old_member: discord.Member, new_member: discord.Member):
+        """
+        When a role is added or removed, look up what category it is in and determine whether to add a header role
+        """
+        if len(old_member.roles) > len(new_member.roles):
+            # Role must have been removed, so find an old role that isn't a new role
+            role = [role for role in old_member.roles if role not in new_member.roles][0]
+        elif len(old_member.roles) < len(new_member.roles):
+            # Role has been added so find a new role that isn't an old role
+            role = [role for role in new_member.roles if role not in old_member.roles][0]
+        else:
+            # Some other kind of update
+            return
+
+        await self.process_roles(new_member, role)
+
+    async def process_roles(self, member: discord.Member, changed_role: discord.Role):
+        for detail in self.role_config.values():
+            divider_id = detail.get("divider")
+            if not divider_id:
+                continue
+
+            roles = [x["id"] for x in detail.get("roles", {}).values()]
+
+            if changed_role.id in roles:
+                # We have found the category this role belongs to
+
+                # Determine if we need to add or remove the divider
+
+                has_roles_in_category = False
+
+                for role_id in roles:
+                    role = member.guild.get_role(role_id)
+                    if role in member.roles:
+                        has_roles_in_category = True
+                        break
+
+                # Apply the changes
+
+                divider = member.guild.get_role(divider_id)
+                if has_roles_in_category and divider not in member.roles:
+                    await member.add_roles(divider, reason="Divider role.")
+                elif not has_roles_in_category and divider in member.roles:
+                    await member.remove_roles(divider, reason="Divider role.")
+
+                # Don't process other categories since we have already matched one
+                return
 
 
 def setup(bot):
